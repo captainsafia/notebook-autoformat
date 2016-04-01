e151432be249b1a95e7479d7f31001a323996ef2
Safia Abdalla <safia@safia.rocks>
2016-03-31 22:29:51 -0500

    Added functionality for format

--- notebook-autoformat.js
+++ notebook-autoformat.js
@@ -1,5 +1,14 @@
 define(['base/js/namespace', 'jquery'], function(Jupyter, $) {
+    function format(text) {
+        return text;
+    }
+
     function format_code_cell() {
+        var selected_index = Jupyter.notebook.get_selected_index();
+        var selected_cell = Jupyter.notebook.get_selected_cell();
+        var selected_text = selected_cell.get_text();
+        var formatted_text = format(selected_text);
+        selected_cell.set_text(formatted_text);
     }
 
     function place_autoformat_button() {
